ae0fd8b82e65fd41e8eb8e3d0b6dde5671185f55
Ivan Tomskiy <vanatomskij088@gmail.com>
2025-03-01 19:13:06 +0300

    add Attachment

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,11 +1,12 @@
 data class Post(
     val id: Int,
-    val ownerId: Int,
+    val ownerId: Int?,
     val fromId: Int,
     val date: Int,
     val text: String,
     val comments: Comments,
     val likes: Likes,
+    val attachment: Attachment?,
     val views: Int = 0,
     val friendsOnly: Boolean = false,
     val canPin: Boolean = false,
@@ -24,12 +25,73 @@ class Comments(
     val canOpen: Boolean = false
 )
 
-class   Likes(
+class Likes(
     val count: Int = 0,
     val userLikes: Boolean = false,
     val canLike: Boolean = true,
     val canPublish: Boolean = true
 )
+
+open class Attachment(
+    val type: String,
+    val id: Int,
+    val ownerId: Int,
+    val date: Int
+)
+
+class Photo(
+    id: Int,
+    ownerId: Int,
+    date: Int,
+    val album_id: Int,
+    val text: String,
+    val width: Int,
+    val height: Int,
+    type: String = "Photo"
+) : Attachment(type, id, ownerId, date)
+
+class Audio(
+    id: Int,
+    ownerId: Int,
+    date: Int,
+    val artist: String,
+    val text: String,
+    val duration: Int,
+    val albumId: Int,
+    type: String = "Audio"
+) : Attachment(type, id, ownerId, date)
+
+class Video(
+    id: Int,
+    ownerId: Int,
+    date: Int,
+    val description: String,
+    val title: String,
+    val duration: Int,
+    val width: Int,
+    val height: Int,
+    type: String = "Video"
+) : Attachment(type, id, ownerId, date)
+
+class File(
+    id: Int,
+    ownerId: Int,
+    date: Int,
+    val size: Long,
+    val title: String,
+    val ext: String,
+    type: String = "File"
+) : Attachment(type, id, ownerId, date)
+
+class Url(
+    id: Int,
+    ownerId: Int,
+    date: Int,
+    val url: String,
+    val title: String,
+    val description: String,
+    type: String = "Url"
+) : Attachment(type, id, ownerId, date)
 
 object WallService {
 
@@ -64,8 +126,10 @@ object WallService {
 fun main() {
     val comments = Comments()
     val likes = Likes()
-    var post1 = Post(1, 2, 3, 2025, "Hello", comments, likes)
-    var post2 = Post(2, 2, 3, 2026, "Hello", comments, likes)
+    val video1: Attachment = Video(1, 1, 2025, "Funny", "Discovery", 1800, 1920, 1080)
+    val file1: Attachment = File(2, 1, 2023, 1024, "project", "pdf")
+    var post1 = Post(1, 2, 3, 2025, "Hello", comments, likes, video1)
+    var post2 = Post(2, 2, 3, 2026, "Hello", comments, likes, file1)
     WallService.add(post1)
     println(WallService.update(post2))
 }
--- src/test/kotlin/WallServiceTest.kt
+++ src/test/kotlin/WallServiceTest.kt
@@ -13,7 +13,8 @@ class WallServiceTest {
     fun addNoNull() {
         val comments = Comments()
         val likes = Likes()
-        val post1 = Post(1, 2, 2, 2025, "Hello", comments, likes)
+        val video1: Attachment = Video(1, 1, 2025, "Funny", "Discovery", 1800, 1920, 1080)
+        val post1 = Post(1, 2, 2, 2025, "Hello", comments, likes, video1)
 
         assertEquals(WallService.add(post1).id != 0, true)
     }
@@ -22,8 +23,10 @@ class WallServiceTest {
     fun updateTrue() {
         val comments = Comments()
         val likes = Likes()
-        val post1 = Post(1, 2, 2, 2025, "Hello",  comments, likes)
-        val post2 = Post(1, 2, 2, 2026, "Hello",  comments, likes)
+        val video1: Attachment = Video(1, 1, 2025, "Funny", "Discovery", 1800, 1920, 1080)
+        val file1: Attachment = File(2, 1, 2023, 1024, "project", "pdf")
+        val post1 = Post(1, 2, 2, 2025, "Hello",  comments, likes, video1)
+        val post2 = Post(1, 2, 2, 2026, "Hello",  comments, likes, file1)
         WallService.add(post1)
         assertEquals(WallService.update(post2), true)
     }
@@ -32,8 +35,10 @@ class WallServiceTest {
     fun updateFalse() {
         val comments = Comments()
         val likes = Likes()
-        val post1 = Post(1, 2, 2, 2025, "Hello",  comments, likes)
-        val post2 = Post(2, 2, 2, 2026, "Hello",  comments, likes)
+        val video1: Attachment = Video(1, 1, 2025, "Funny", "Discovery", 1800, 1920, 1080)
+        val file1: Attachment = File(2, 1, 2023, 1024, "project", "pdf")
+        val post1 = Post(1, 2, 2, 2025, "Hello",  comments, likes, file1)
+        val post2 = Post(2, 2, 2, 2026, "Hello",  comments, likes, video1)
         WallService.add(post1)
         assertEquals(WallService.update(post2), false)
     }
